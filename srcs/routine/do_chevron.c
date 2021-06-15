8fdeb8dac3a693210b02d5c24c75e4565356eca9
Florian Cadet <cadet.florian@gmail.com>
2021-06-15 13:07:20 +0200

    double chevron

--- srcs/routine/do_chevron.c
+++ srcs/routine/do_chevron.c
@@ -36,11 +36,9 @@ static void	double_chevron(char **cmd, t_struct *st, int i)
 	new_fd = open(cmd[i + 1], O_WRONLY | O_APPEND | O_CREAT, 0666);
 	if (new_fd < 0)
 		return ;
-	close(STDOUT_FILENO);
-	if (dup(new_fd) < 0)
+	if (dup2(new_fd, STDOUT_FILENO) < 0)
 		return ;
-	st->stdout_fd = new_fd;
-	close(new_fd);
+	st->was_chevr = 1;
 }
 
 static int	left_chevron(char **cmd, t_struct *st, int i)
